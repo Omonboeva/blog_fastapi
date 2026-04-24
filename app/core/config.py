9a38f7a21839e99f21f86b330160da641af2f9ea
nargiza <omonboyeva00@gmail.com>
2026-04-24 16:44:50 +0500

    dars

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic import AnyUrl
 
 class Settings(BaseSettings):
     DATABASE_URL: str = "postgresql+asyncpg://postgres:123@localhost:5432/blog_db"
-    SECRET_KEY: str = ""
+    SECRET_KEY: str = "59c48aaad8fb52387fd9bf562cc9edb1ff50660578fcf19c0b891d7178017f43"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
